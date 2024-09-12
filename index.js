af035cac599c55819e4fae23601bf48c31b77f07
dunglt94 <tuandung9594@gmail.com>
2024-09-12 15:31:52 +0700

    fix js code

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 function convert() {
-    let amount = document.getElementById("amount").value;
-    let r = amount / 23000;
-    document.getElementById("result").innerText = `Result: ${r} $`;
+    let amount = +document.getElementById("amount").value;
+    let from = +document.getElementById("from").value;
+    let to = +document.getElementById("to").value;
+    let exchange_rate = to / from
+    let result = amount * exchange_rate;
+    document.getElementById("result").innerText = `Result: ${result}`;
 }
-
-//document.write("Result: " + r + " $");
